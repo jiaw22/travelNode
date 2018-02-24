dd049e2e15d231a31a1386f81b9b0765c5eebc1c
Shirley <szk4@cornell.edu>
2018-02-24 12:50:14 -0500

    Fix everything

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
+app = Flask(__name__)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return "Flask App!"
+
+@app.route("/hello/<string:name>/")
+def hello(name):
     return render_template(
         'index.html',name=name)
 
-@app.route("/route")
-def hello():
-    return "Hello World!"
-
-@app.route("/suggestions")
-def members():
-    return "Members"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
 
 # @app.route("/members/<string:name>/")
 # def getMember(name):
 #     return name
-
-if __name__ == "__main__":
-    app.run(host='travelNode.tech', port=80)
