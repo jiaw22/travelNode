f782298d665c918018cadb916deb14895cb46a7a
Shirley <szk4@cornell.edu>
2018-02-24 16:40:33 -0500

    Fix backgroudn

--- app/templates/js.js
+++ app/templates/js.js
@@ -1,8 +1,12 @@
-$("#activeslider").slider({step: 100000, min: 0, max: 200000});
+$("#activeslider").slider({
+  step: 100000,
+  min: 0,
+  max: 200000
+});
 
 // Without JQuery
 var slider = new Slider("#activeslider", {
-	step: 100000,
-	min: 0,
-	max: 200000
+  step: 100000,
+  min: 0,
+  max: 200000
 });
