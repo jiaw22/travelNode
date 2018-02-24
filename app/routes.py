9e8298b26454279bd5a1d9711fd913f0f8ddc03f
Shirley <szk4@cornell.edu>
2018-02-24 13:56:15 -0500

    Fix login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template, flash, redirect
 from app import app
 
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
