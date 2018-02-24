14fe2e80deca402acd71300ec3e8a05534cf5a4e
riddhima25 <riddhima.narravula@gmail.com>
2018-02-24 17:49:39 -0500

    Added button to add locations

---
+++ app/templates/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var max_fields      = 5; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" id=location'+x+' name="mytext[]" placeholder="Fill in location"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
