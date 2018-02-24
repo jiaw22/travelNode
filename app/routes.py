338c101c4c4f02d28f5bfd8008df3b0699c79478
Shirley <shirleykabir@gmail.com>
2018-02-24 17:49:46 -0500

    Plz work

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template, flash, redirect
 from app import app
-from app.models import User
 from flask import render_template
-from app import app
-
+from flask import request, jsonify
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('index.html', title='Travel Faster, Travel Better')
+
+@app.route('/display', methods=["GET", "POST"])
+def display():
+     result = request.form
+     return jsonify(request.form)
