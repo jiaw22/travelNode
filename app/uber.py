1b1dee2880b318ffcb1f72771d6ee14e9c94be44
Shirley <shirleykabir@gmail.com>
2018-02-25 02:11:42 -0500

    priority queue djik

--- app/uber.py
+++ app/uber.py
@@ -6,7 +6,7 @@ session = Session(server_token="wJFmlljkrczBkDB7k9_CPOxhgmX2KaiLd-bxlQPb")
 client = UberRidesClient(session)
 
 #returns two outputs: duration time in seconds and cost in dollars
-def findCostAndTime (startLat,startLong,endLat,endLong,seat = 2):
+def findCostAndTime (startLat,startLong,endLat,endLong,seat = 1):
     response = client.get_price_estimates(
     start_latitude= startLat,
     start_longitude= startLong,
@@ -32,3 +32,5 @@ def findCostAndTime (startLat,startLong,endLat,endLong,seat = 2):
 returnedDuration, returnedTime = findCostAndTime(37.770,-122.411,37.791,-122.405)
 print returnedTime
 print returnedDuration
+
+# walking https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101%2C-73.89188969999998&destinations=40.6905615%2C-73.9976592&mode=walking&key=AIzaSyBHMScNZ7yXQ2PJ-yFkf4qmc7qQuzAQQWM
