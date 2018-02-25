d1678f7d3014737dbd71f3ce5ea92d4eea55fd96
jiaw22 <jw984@cornell.edu>
2018-02-24 22:41:09 -0500

    Made it into a function that returns two outputs:

--- app/uber.py
+++ app/uber.py
@@ -6,24 +6,28 @@ session = Session(server_token="wJFmlljkrczBkDB7k9_CPOxhgmX2KaiLd-bxlQPb")
 client = UberRidesClient(session)
 
 #hard-coded values but should be able to be changed
-response = client.get_price_estimates(
-    start_latitude=37.770,
-    start_longitude=-122.411,
-    end_latitude=37.791,
-    end_longitude=-122.405,
+def findCostAndTime (startLat,startLong,endLat,endLong,seat = 2):
+    response = client.get_price_estimates(
+    start_latitude= startLat,
+    start_longitude= startLong,
+    end_latitude= endLat,
+    end_longitude= endLong,
     seat_count=2
-)
+    )
 
-#list of all the diff possibilities of ubers
-estimate = response.json.get('prices')
+    #list of all the diff possibilities of ubers
+    estimate = response.json.get('prices')
 
-#ubers select option
-first_JSon = estimate[0]
+    #ubers select option
+    first_JSon = estimate[0]
 
-#duration time in seconds
-#estimate cost is in dollars
-duration_time = first_JSon['duration']
-estimate_cost = first_JSon['high_estimate']
+    #duration time in seconds
+    #estimate cost is in dollars
+    duration_time = first_JSon['duration']
+    estimate_cost = first_JSon['high_estimate']
 
-print duration_time
-print estimate_cost
+    return duration_time, estimate_cost;
+#figure out what to do for error messages
+returnedDuration, returnedTime = findCostAndTime(37.770,-122.411,37.791,-122.405)
+print returnedTime
+print returnedDuration
