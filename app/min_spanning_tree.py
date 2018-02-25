e7c1f2d4999fcc67f4e1ce2ae96a406997fd0633
Shirley <shirleykabir@gmail.com>
2018-02-25 10:38:10 -0500

    E

--- app/min_spanning_tree.py
+++ app/min_spanning_tree.py
@@ -13,16 +13,20 @@ class Graph():
 
     # A utility function to print the constructed MST stored in parent[]
     def printMST(self, parent):
+        route = []
         print "Edge \tWeight"
         for i in range(1,self.V):
             print parent[i],"-",i,"\t",self.graph[i][ parent[i] ]
+            route.append(parent[i])
+        return route
 
-    def getPath(self, parent):
-        route = [(0,0)] #(next node, weight)
+    '''def getPath(self, parent):
+        route = []
         #print "Edge \tWeight \tCost"
         for i in range(1,self.V):
-            route.append((i,self.graph[i][parent[i]]))
+            route.append(parent[i])
         return route
+    '''
     # A utility function to find the vertex with minimum distance value, from
     # the set of vertices not yet included in shortest path tree
     def minKey(self, key, mstSet):
@@ -71,8 +75,8 @@ class Graph():
             #print(parent)
 
         #print key
-        route = (self.getPath(parent))
-        return route
+        #route = (self.getPath(parent))
+        #return route
 
 #PARAMETERS
 
@@ -85,84 +89,80 @@ class Graph():
 
 #for each location, choose either car time or
 #returns: (i.e. for 'A')[0 30 10 20 4]
-# data1 = [(u'232 East Ave, Ithaca, NY 14850, USA', u'111 Dryden Rd, Ithaca, NY 14850, USA', 120, 9.0, 849, 0.7)]
-# time = [(u'232 East Ave, Ithaca, NY 14850, USA', u'50'), (u'111 Dryden Rd, Ithaca, NY 14850, USA', u'50')]
-# own_vehicle = "no"
-# max_time = 4
-# activity_level = 50
-# '''
-# def construct_whole(data, time, own_vehicle, max_time, activity_level):
-#     g = Graph(len(time))
-#     matrix = []
-#     for i in range(len(time)): #for every row
-#         row = construct_row(data1, time, own_vehicle, max_time, activity_level) #populating a row
-#         matrix.append(row)
-#     g.graph = matrix
-#     g.primMST();
-# '''
+data1 = [(u'232 East Ave, Ithaca, NY 14850, USA', u'111 Dryden Rd, Ithaca, NY 14850, USA', 120, 9.0, 849, 0.7)]
+time = [(u'232 East Ave, Ithaca, NY 14850, USA', u'50'), (u'111 Dryden Rd, Ithaca, NY 14850, USA', u'50')]
+own_vehicle = "no"
+max_time = 4
+activity_level = 50
+'''
+def construct_whole(data, time, own_vehicle, max_time, activity_level):
+    g = Graph(len(time))
+    matrix = []
+    for i in range(len(time)): #for every row
+        row = construct_row(data1, time, own_vehicle, max_time, activity_level) #populating a row
+        matrix.append(row)
+    g.graph = matrix
+    g.primMST();
+'''
 def construct_whole(data1, time, own_vehicle, max_time, activity_level): #num_locs, personal_car, act_lvl
-    leg_info = """{"addresses": ['+ locA + ', '+ locB +', '+ locC +', '+ locD + ', '+ locE + '],
-      "leg1": {
-        "transport": ' + transpA + ',
-        "legTime": ' + timeA + ',
-        "legDist": ' + distA + ',
-        "cost": ' + costA + '
-      },
-      "leg2": {
-        "transport": ' + transpB + ',
-        "legTime": ' + timeB + ',
-        "legDist": ' + distB + ',
-        "cost": ' + costB + '
-      },
-      "leg3": {
-        "transport": ' + transpC + ',
-        "legTime": ' + timeC + ',
-        "legDist": ' + distC + ',
-        "cost": ' + costC + '
-      },
-      "leg4": {
-        "transport": ' + transpD + ',
-        "legTime": ' + timeD + ',
-        "legDist": ' + distD + ',
-        "cost": ' + costD + '
-      },
-      "totaltime" : ' + acc_time + ',
-      "totaldistance": ' + totaldist + ',
-      "totalcost": ' + totalcost + ',
-      "warning": ' + warning + '
-    }"""
     #print time
     g = Graph(len(time))
     matrix = []
+    accessories = []
     for i in range(len(time)):
         row = []
-        car_cost = []
-        using_uber = []
+        acc = []
+        #car_cost = []
+        #using_uber = []
         for j in range(len(time)): #for every column
             est_loc_time = time[j][1]
             #print type(time)
             if (((own_vehicle == 'Yes') | (activity_level < 100)) | (est_loc_time > 5)): #loc_est_time
                 t = data1[0][3] #Uber time
-                using_uber.append(data1[0][2])
+                a = ["Uber", est_loc_time, data1[0][5], data1[0][2]]
+                #using_uber.append(data1[0][2])
             else:
                 t = data1[0][4] #walking time
                 if (j==i):
                     t = 0
-                using_uber.append(0)
+                a = ["Walk", est_loc_time, data1[0][5], 0]
 
             row.append(t)
-            car_cost.append(using_uber)
+            acc.append(a)
+            #car_cost.append(using_uber)
         matrix.append(row)
+        accessories.append(acc)
+
     g.graph = matrix
-    route = g.primMST()#, leg_info
+    node_order = g.primMST()
 
-    print car_cost
-    ultimate_route = []
-    for i in range(len(route)):
-        ultimate_route.append((route[i],car_cost[i]))
-    print ultimate_route
+    acc_time = accessories[node_order[0]][node_order[1]][1] + accessories[node_order[1]][node_order[2]][1]+accessories[node_order[2]][node_order[3]][1]+accessories[node_order[3]][node_order[4]][1]
 
-# construct_whole(data1, time, own_vehicle, max_time, activity_level)
+    total_dist = accessories[node_order[0]][node_order[1]][2] +accessories[node_order[1]][node_order[2]][2] +accessories[node_order[2]][node_order[3]][2] +accessories[node_order[3]][node_order[4]][2]
+
+    total_cost = accessories[node_order[0]][node_order[1]][3] +accessories[node_order[1]][node_order[2]][3] +accessories[node_order[2]][node_order[3]][3] +accessories[node_order[3]][node_order[4]][3]
+
+    if acc_time > max_time:
+        warning = "OVERTIME"
+    else:
+        warning = "NO WARNING"
+
+    leg_info = '{"addresses": [' + time(node_order[0]) + '", "' + time(node_order[1]) + '", "'+ time(node_order[2]) + '", "'+ time(node_order[3]) + '", "' + time(node_order[4]) +'"],'
+    leg1 = '"leg1": { "transport": ' + accessories[node_order[0]][node_order[1]][0] + ', "legTime": ' + accessories[node_order[0]][node_order[1]][1] + ', "legDist": ' + accessories[node_order[0]][node_order[1]][2] + ', "cost": ' + accessories[node_order[0]][node_order[1]][3]
+    leg1 =  leg1 + '"},'
+    leg2 = '"leg2": { "transport": ' + accessories[node_order[1]][node_order[2]][0] + ', "legTime": ' + accessories[node_order[1]][node_order[2]][1] + ', "legDist": ' + accessories[node_order[1]][node_order[2]][2] + ', "cost": ' + accessories[node_order[1]][node_order[2]][3]
+    leg2 =  leg2 + '"},'
+    leg3 = '"leg3": { "transport": ' + accessories[node_order[2]][node_order[3]][0] + ', "legTime": ' +  accessories[node_order[2]][node_order[3]][1] + ', "legDist": ' + accessories[node_order[2]][node_order[3]][2] + ', "cost": ' + accessories[node_order[2]][node_order[3]][3]
+    leg3 =  leg3 + '"},'
+    leg4 = '"leg4": { "transport": ' + accessories[node_order[3]][node_order[4]][0] + ', "legTime": ' +  accessories[node_order[3]][node_order[4]][1] + ', "legDist": ' + accessories[node_order[3]][node_order[4]][2] + ', "cost": ' + accessories[node_order[3]][node_order[4]][3]
+    leg4 =  leg4 + '"},'
+    ending =  '"totaltime" : ' + acc_time + ',"totaldistance": ' + totaldist + ',"totalcost": ' + totalcost + ',"warning": ' + warning
+    ending = ending + '}"'
+
+    finish =  leg_info + leg1 + leg2 + leg3 + leg4 + ending
+    print finish
+
+construct_whole(data1, time, own_vehicle, max_time, activity_level)
 #g.graph = matrix
 #g.primMST();
 
@@ -170,7 +170,7 @@ def construct_whole(data1, time, own_vehicle, max_time, activity_level): #num_lo
 #    warning = True
 
 #str that was supposed to be a json file ....
-# leg_info = """{"addresses": ['+ locA + ', '+ locB +', '+ locC +', '+ locD + ', '+ locE + '],
+# leg_info = """{"addresses": ['locA ', 'locB', 'locC', 'locD ', '+ locE + '],
 #   "leg1": {
 #     "transport": ' + transpA + ',
 #     "legTime": ' + timeA + ',
